b76074883529890688124e52db4d875b8efb7a65
Smolderbrand <59597833+Smolderbrand@users.noreply.github.com>
2024-05-12 11:12:52 +0300

    Variable name change
    First project

--- ProjectLights/script.js
+++ ProjectLights/script.js
@@ -1,20 +1,20 @@
-var semaphor;
+var semaphorColor;
 
 function start() {
 	document.getElementById("button").style.background = "#FF0000";
-	window.semaphor = 0;
+	window.semaphorColor = 0;
 	var intervalId = setInterval(function() { clickButton() }, 10000);
 }
 
 function clickButton() {
-	if (window.semaphor == 0) {
+	if (window.semaphorColor == 0) {
 		document.getElementById("button").style.background = "#FFFF40";
-		window.semaphor = 1;
-	} else if (window.semaphor == 1) {
+		window.semaphorColor = 1;
+	} else if (window.semaphorColor == 1) {
 		document.getElementById("button").style.background = "#00C000";
-		window.semaphor = 2;
-	} else if (window.semaphor == 2) {
+		window.semaphorColor = 2;
+	} else if (window.semaphorColor == 2) {
 		document.getElementById("button").style.background = "#FF0000";
-		window.semaphor = 0;
+		window.semaphorColor = 0;
 	}
 }
